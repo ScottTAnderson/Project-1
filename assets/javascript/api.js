bf84056afa6ffe215057cbd54c4f5fc8b138f4e3
ScottTAnderson <42193132+ScottTAnderson@users.noreply.github.com>
2019-01-28 17:53:25 -0600

    working random movie finder and random recipe finder

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -20,22 +20,24 @@
 //         });
 // }
 
+var recipeURL = '';
+
 function getRandomMovie() {
     var apiKey = '5eac88493bbb29ff93bb4bedf09e7f4e';
-    var randomMovie = Math.floor(Math.random()*20 + 1); 
-    var randomPage =  Math.floor(Math.random()*20 + 1);
+    var randomMovie = Math.floor(Math.random() * 20 + 1);
+    var randomPage = Math.floor(Math.random() * 20 + 1);
     console.log(randomMovie);
     console.log(randomPage);
     //API Read Access Token = eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZWFjODg0OTNiYmIyOWZmOTNiYjRiZWRmMDllN2Y0ZSIsInN1YiI6IjVjNGU3ZDliMGUwYTI2NDk1YWQ3NzJmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q6gP_DxVZvU1_9xztOOuJ_5pATqDkg33cWwVgSCkyyQ
     var queryURL = 'https://api.themoviedb.org/3/discover/movie?api_key=' +
-    apiKey +
-    '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' +
-    randomPage +
-    '&primary_release_date.gte=' + 
-    1980 +
-    //$('.startYear').val() +  //use the input value for start year in HTML
-    '&primary_release_date.lte=' + 
-    1990;
+        apiKey +
+        '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' +
+        randomPage +
+        '&primary_release_date.gte=' +
+        1980 +
+        //$('.startYear').val() +  //use the input value for start year in HTML
+        '&primary_release_date.lte=' +
+        1990;
     //$('.endYear').val(); //use the input value for end year in HTML
     console.log(queryURL);
 
@@ -46,10 +48,36 @@ function getRandomMovie() {
         method: 'GET',
         // headers: {},
         // data: '{}'
-    }).then(function (response){
+    }).then(function (response) {
         console.log(response.results[randomMovie]);
     })
 }
 
+function getRecipeJson() {
+    var apiKey = '01ec3a6106be7bd086339dcba3f5c9af';
+    var recipeID = 'chicken';
+    var randomRecipe = Math.floor(Math.random() * 30 + 1);
+    var randomPage = Math.floor(Math.random() * 5 + 1);
+    //var recipeID = $('.protienChoice').val(); // to change querry to something based on user input
+    var queryURL = 'https://www.food2fork.com/api/search?key=' + apiKey + '&q=' + recipeID + '&page=' + randomPage;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        var jsonResponse = JSON.parse(response)
+        recipeURL = jsonResponse.recipes[randomRecipe].source_url;
+        $('.recipe').attr("src", recipeURL);
+            console.log(jsonResponse);        
+            console.log(recipeURL);
+    });
+}
+
+
+        
+
+$(document).ready(function() {
 getRandomMovie();
-getRandomMovie();
+getRecipeJson();
+
+});
+//Food2Fork API key = 01ec3a6106be7bd086339dcba3f5c9af max usage 50
