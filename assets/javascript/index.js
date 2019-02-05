539d6fa44173472c3b62546286388e2d38831930
ScottTAnderson <42193132+ScottTAnderson@users.noreply.github.com>
2019-02-05 15:44:44 -0600

    added modal login

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -13,3 +13,39 @@ var granimInstance = new Granim({
         }
     }
 });
+
+$('.navbar-brand').on('click', function(){
+    $('.modal').css('display', 'block');
+});
+
+$('.close').on('click', function() {
+    $('.modal').css('display', 'none');
+})
+
+// // Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("button");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+//   console.log('success');
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+// })
